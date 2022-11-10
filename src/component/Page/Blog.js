c266d17f1a2bcebc7145c88c634eae849ab743a9
Janardhan Majumder <subrotomojumder03@gmail.com>
2022-11-10 14:37:49 +0600

    answer and question complete

--- src/component/Page/Blog.js
+++ src/component/Page/Blog.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import { useTitle } from '../../Hooks/UseTitle';
 
 const Blog = () => {
+    useTitle('Blog')
     return (
-        <div className='mx-8 lg:mx-16'>
+        <div className='mx-8 lg:mx-16 mb-10'>
             <h2 className='text-center mt-6 text-2xl text-cyan-600'>Question and Answer</h2>
             <div>
                 <h5 className='text-lg font-semibold mt-4'>Q.1. Difference between SQL and NoSQL?</h5>
-                <blog><span className='font-bold text-blue-400'>Ans: </span> SQL, NoSQL
+                <blog className='ml-3'><span className='font-bold text-blue-400'>Ans: </span> SQL, NoSQL
                     When it comes to choosing a database the biggest decisions is picking a relational (SQL) or non-relational (NoSQL)
                     data structure. While both the databases are viable options still there are certain key differences between.
                     <br />
@@ -39,7 +41,7 @@ const Blog = () => {
             </div>
             <div>
                 <h5 className='text-lg font-semibold mt-4'>Q.2. What is JWT, and how does it work?</h5>
-                <blog><span className='font-bold text-blue-400'>Ans: </span> JSON Web Token, is an open standard used to share security information between two parties — a
+                <blog className='ml-3'><span className='font-bold text-blue-400'>Ans: </span> JSON Web Token, is an open standard used to share security information between two parties — a
                     client and a server. Each JWT contains encoded JSON objects, including a set of claims. JWTs are signed using a cryptographic algorithm to ensure that the
                     claims cannot be altered after the token is issued.
                     <br />
@@ -47,13 +49,33 @@ const Blog = () => {
                     depends on the use case at hand. For example, a claim may assert who issued the token, how long it is valid for, or what permissions the client has been granted.
                     A JWT is a string made up of three parts, separated by dots (.), and serialized using base64. The signature.
                     The JOSE (JSON Object Signing and Encryption) header contains the type of token — JWT in this case — and the signing algorithm. The signature ensures that the token hasn’t
-                     been altered. The party that creates the JWT signs the header and payload with a secret that is known to both the issuer and receiver, or with a private key known only to 
-                     the sender. When the token is used, the receiving party verifies that the header and payload match the signature.
+                    been altered. The party that creates the JWT signs the header and payload with a secret that is known to both the issuer and receiver, or with a private key known only to
+                    the sender. When the token is used, the receiving party verifies that the header and payload match the signature.
                 </blog>
             </div>
             <div>
-                <h5 className='text-lg font-semibold mt-4'>Q.2. What is the difference between javascript and NodeJS?</h5>
-                <blog><span className='font-bold text-blue-400'>Ans: </span> 
+                <h5 className='text-lg font-semibold mt-4'>Q.3. What is the difference between javascript and NodeJS?</h5>
+                <blog className='ml-3'><span className='font-bold text-blue-400'>Ans: </span>
+                    <p className='ml-7 mt-2'> 1. JavaScript is a client-side scripting language that is lightweight, cross-platform, and interpreted. Both Java and HTML include it. Node.js,
+                        on the other hand, is a V8-based server-side programming language.
+                    </p>
+                    <p className='ml-7 mt-2'> 2. JavaScript is a simple programming language that can be used with any browser that has the JavaScript Engine installed. Node.js, on the other
+                        hand, is an interpreter or execution environment for the JavaScript programming language. It requires libraries that can be conveniently accessed from JavaScript programming to be more helpful.
+                    </p>
+                    <p className='ml-7 mt-2'> 3. Any engine may run JavaScript. As a result, writing JavaScript is incredibly easy, and any working environment is similar to a complete browser. Node.js, on the other hand,
+                        only enables the V8 engine. Written JavaScript code, on the other hand, can run in any context, regardless of whether the V8 engine is supported.
+                    </p>
+                    <p className='ml-7 mt-2'> 4. A specific non-blocking operation is required to access any operating system. There are a few essential objects in JavaScript, but they are entirely OS-specific.
+                        <br /> Node.js, on the other hand, can now operate non-blocking software tasks out of any JavaScript programming. It contains no OS-specific constants.
+                    </p>
+                </blog>
+            </div>
+            <div>
+                <h5 className='text-lg font-semibold mt-4'>Q.4. How does NodeJS handle multiple requests at the same time?</h5>
+                <blog className='ml-3'><span className='font-bold text-blue-400'>Ans: </span>NodeJS receives multiple client requests and places them into EventQueue. NodeJS is built with the concept
+                    of event-driven architecture. NodeJS has its own EventLoop which is an infinite loop that receives requests and processes them. EventLoop is the listener for the EventQueue.
+                    If NodeJS can process the request without I/O blocking then the event loop would itself process the request and sends the response back to the client by itself. But, it is possible to process multiple
+                    requests parallelly using the NodeJS cluster module or worker_threads module.
                 </blog>
             </div>
         </div>
